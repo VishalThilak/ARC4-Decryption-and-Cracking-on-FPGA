0e2b3fa6dfb593c30d6aa42f2f6a9fe361726b05
Vishal Thilak <93748287+VishalThilak@users.noreply.github.com>
2025-01-27 14:58:03 -0800

    Update and rename phase2.sv to task2.sv

--- task2.sv
+++ task2.sv
@@ -1,4 +1,4 @@
-module phase2(input logic CLOCK_50, input logic [3:0] KEY, input logic [9:0] SW,
+module task2(input logic CLOCK_50, input logic [3:0] KEY, input logic [9:0] SW,
              output logic [6:0] HEX0, output logic [6:0] HEX1, output logic [6:0] HEX2,
              output logic [6:0] HEX3, output logic [6:0] HEX4, output logic [6:0] HEX5,
              output logic [9:0] LEDR);
